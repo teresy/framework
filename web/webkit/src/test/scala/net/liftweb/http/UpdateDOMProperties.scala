aa778c192ea89304a7a234eaf8de2a6a55667bc5
Joe Barnes <barnesjd@gmail.com>
2016-09-14 13:04:41 -0500

    Making the VDOM generative property tests more difficult by allowing 1 - 5 mutations

--- web/webkit/src/test/scala/net/liftweb/http/UpdateDOMProperties.scala
+++ web/webkit/src/test/scala/net/liftweb/http/UpdateDOMProperties.scala
@@ -93,7 +93,7 @@ object UpdateDOMProperties extends Properties("UpdateDOM") {
     isntWhitespace)
 
   property("UpdateDOM should handle an arbitrary mutation of our static template") = forAll(
-    VDomGen.genMutation(template, List(2, 3, 4, 5, 10, 11, 12, 13, 14, 15, 16), List(1, 11))
+    VDomGen.genMutations(template, List(2, 3, 4, 5, 10, 11, 12, 13, 14, 15, 16), List(1, 11))
   ) { after =>
     UpdateDOMSpec.roundTrip(template, after) == after
   }
